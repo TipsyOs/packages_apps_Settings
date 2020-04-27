1ba7dd5264b8ca2ea497a6ece41ff11398bb8e7b
alliancesparx <alliancesparx@gmail.com>
2020-04-27 09:11:19 +0100

    Settings: Tipsy metrics

--- src/com/android/settings/display/DisplayRotation.java
+++ src/com/android/settings/display/DisplayRotation.java
@@ -61,7 +61,7 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.CARBONFIBERS;
+        return MetricsEvent.TIPSY;
     }
 
     @Override
